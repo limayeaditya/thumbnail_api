ee91e0f486effddbf5ef1502b9f67ac4a14add37
adil8539 <52648711+adil8539@users.noreply.github.com>
2020-05-17 00:24:53 +0530

    Improvised Code

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ var jwt = require('jsonwebtoken');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
+
+//include token verification and thumbnail generation
 var middlewares = require('./middlewares');
+
+//secret key: requisite for token generation
 const key = require('./key');
-
-
-require('dotenv').config();
 
 app.use(express.static('thumbnails'));
 
-
-
+//Renders the login screen on http://localhost:3000/
 app.get('/', function(req, res){
    res.render('login');
 });
-
+//Sets the default view engine to pug
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -32,16 +32,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array()); 
 app.use(express.static('public'));
 
+//fetch the username and generate token for authentication
 app.post('/', function(req, res){
     const user = req.body.user.toLowerCase();
+    const password = req.body.password;
     let token = jwt.sign(user, key.secret);
-    
-    res.status(200).send({token: token});
+    //pass the token as response
+    res.status(200).json({token: token, user: user, authorization: true});
 
 });
 
+//initially verifies the token and then generates the thumbnail
 app.get('/image',middlewares.validity,middlewares.ThumbnailCreation,(req,res)=>{
-	res.send("Successfully downloaded and thumbnail created");
+	res.send("Successfully created and downloaded the thumbnail");
 	
 });
+
+//listening on port 3000
 app.listen(3000);
--- middlewares.js
+++ middlewares.js
@@ -3,63 +3,63 @@ const jwt     = require('jsonwebtoken');
 const key = require('./key');
 const download = require('image-downloader');
 const fs = require('fs');
-
 const resizeImg = require('resize-img');
 
-
-
+//check if token is valid
 const validity = (req,res,next)=>{
 	const {token} = req.headers;
 	if(token){
 		jwt.verify(token, key.secret, function(err, decoded) {
 			if (err) {
 				res.send('unauthorized');
-
+				//token invalid
 			}
 			else{
 				next();
+				//go for thumbnail generation
 			}
 		});
 	}
 	else{
-		res.send('Not good')
+		res.send('Token missing')
+		//token is missing
 	}
 	
 };
 
 const ThumbnailCreation = (req,res,next)=>{
 	
-				
+	let url = req.body.url; //image URL 
+	let ext = path.extname(url).trim(); //trims the image name from the URL
 	
-
-
-	let url = req.body.url;
-	let ext = path.extname(url).trim();
-	console.log(ext)
+	//check for valid file extensions
 	if(ext === '.bmp' || ext === '.jpg' || ext ==='.png'){
-		//generate
+		
+		//download options
 		const options = {
 		 url: url,
 		 dest: './'
 		};
 
-		download.image(options)
+		download.image(options) // download image
 		.then(({ filename, image }) => {
-			console.log(filename);
+			
 			resizeImg(fs.readFileSync(filename), {width:50, height:50}).then(buf => {
-				fs.writeFileSync("./thumbnails/"+filename, buf);
-				
-				res.json({thumbnail: './'+filename});
+				fs.writeFileSync("./thumbnails/"+filename, buf); //download the image resized to thumbnail
+		
+				res.json({thumbnail: './'+filename}); //send json including link to new thumbnail as response
 			next();
 		}).catch((err) => {
-			res.send("Error");
+			res.send("Error"); //error found
+			
 		});
  
 	})
 	}
 
 	else{
-		res.send('File extensions allowed- [bmp,png,jpg]');
+
+		res.send('File extensions allowed- [bmp,png,jpg]'); //response for invalid file extension
 	}
 
 };
